afd1f83f45cdcd22987abbb3305abadd02db8bfa
Kahf Hussain <kahfnhussain@gmail.com>
2023-02-19 19:45:05 +0100

    Combining user and book model with access features

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_19_165224) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_19_181623) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
