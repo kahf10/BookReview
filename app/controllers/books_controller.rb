73789c5af1950f7cf5c8d6457bdab58855eea093
Kahf Hussain <kahfnhussain@gmail.com>
2023-02-19 14:50:03 +0100

    Creating Books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,34 @@
 class BooksController < ApplicationController
 	def index 
 	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+
+		if @book.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end 
+
+	def edit
+	end
+
+	def update
+	end 
+
+	def destroy
+	end 
+
+	private 
+
+		def book_params
+			params.require(:book).permit(:title, :description, :author)
+		end
 end
+ 
