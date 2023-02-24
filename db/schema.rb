0f1acf3e7d83cabccfddf58817074a12c2a7ea58
Kahf Hussain <kahfnhussain@gmail.com>
2023-02-24 18:21:54 +0100

    Added category dropdown list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_19_181623) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_164509) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_19_181623) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
